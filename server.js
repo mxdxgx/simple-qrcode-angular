b8dfcf0a673f0d309ed5cb1d4c64fdff5207951d
mxdx <Capitainecosmos@protonmail.com>
2022-10-07 23:12:12 -0400

    feat: ssr

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.static(__dirname + "/dist/qr-code-web")); // Link index.html of build folder with router.
+app.get("/*", (req, res, next) => {
+  res.sendFile(path.join(__dirname + "/dist/qr-code-web/index.html"));
+});
+app.listen(process.env.PORT || 8000);
