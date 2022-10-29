cde033614896224ea44ceaf1cd06781475a9bfd4
mxdx <Capitainecosmos@protonmail.com>
2022-10-28 23:32:33 -0400

    improve: refactored into component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { QrGeneratorComponent } from './qr-generator/qr-generator.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, QrGeneratorComponent],
   imports: [FormsModule, BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
